ef7052dd3da55c2f1d0f9cc26c08bcc28b182bf5
Simon Njenga <makuno.biz@gmail.com>
2020-01-17 11:14:24 +0300

    Trading signals fully implemented

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8" />
 	<title>{{ env('APP_NAME')  }}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta content="Hullaa" name="description" />
+	<meta content="{{ env('APP_TAG_LINE') }}" name="description" />
 	<meta content="Themesdesign" name="author" />
 	<!-- App favicon -->
 	<link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
